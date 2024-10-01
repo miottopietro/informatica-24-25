3ddb39231abab7af1c997a41a1420e8545d3ec88
miottopietro <pietro.miotto@galileiostiglia.edu.it>
2024-10-01 07:34:45 +0000

    es 2 del01102024 finito

--- 01_20241001.C
+++ 01_20241001.C
@@ -5,19 +5,20 @@
 int main(){
 
     int valore=0;
-    float frattoTre=0;
-    float frattoCinque=0;
 
-    printf("inserisci un valore:");
+    printf("inserisci un valore: ");
     scanf("%d", &valore);
 
-    frattoTre = valore/3;
-    frattoCinque = valore/5;
-
-    if(frattoTre %=!0,frattoCinque %=!0){
-        printf("il valore è divisibile sia per 3 e 5");
+    if(valore % 3 == 0){
+        if(valore % 5 ==0){
+            printf("il valore è divisibile sia per 3 e per 5");
+        }
+        else{
+            printf("il valore non è divisibile sia per 3 e per 5");
+        }
     }
     else{
-        printf("il valore non è divisibile sia per 3 e 5");
+        printf("il valore non è divisibile sia per 3 e per 5");
     }
+    return 0;
 }
