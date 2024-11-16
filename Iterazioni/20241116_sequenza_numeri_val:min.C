97228f81a96b785aa82fdc44ce10c048153648da
miottopietro <pietro.miotto@galileiostiglia.edu.it>
2024-11-16 16:41:58 +0000

    es fin

--- Iterazioni/20241116_sequenza_numeri_val:min.C
+++ Iterazioni/20241116_sequenza_numeri_val:min.C
@@ -1,2 +1,32 @@
 /*leggi un programma che legge una sequenza di numeri interi terminanti con 0 (cioè il programma termina quando viene inserito 0) 
-e ne ricerca il valore minimo visualizzandolo sullo schermo.*/
+e ne ricerca il valore nummin visualizzandolo sullo schermo.*/
+
+#include <stdio.h>
+#include <limits.h>
+int main(){
+
+    int num=0;
+    int nummin=INT_MAX;
+
+    printf("inserisci un numero: ");
+    scanf("%d", &num);
+
+    while(num!=0){
+        printf("il numero inserito è: %d\n", num);
+
+        printf("inserisci un numero: ");
+        scanf("%d", &num);
+
+        if(num<nummin){
+            nummin=num;
+        }
+    }
+
+    if (nummin == INT_MAX) {
+        printf("Nessun numero è stato inserito.\n");
+    } else {
+        printf("Il valore minore è: %d\n", nummin);
+    }
+
+    return 0;
+}
