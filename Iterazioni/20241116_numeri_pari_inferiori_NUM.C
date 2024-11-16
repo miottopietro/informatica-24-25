c7c873e826f756298d561a7d46a757851607507f
miottopietro <pietro.miotto@galileiostiglia.edu.it>
2024-11-16 16:59:02 +0000

    es numeri pari inferiroir a num

--- Iterazioni/20241116_numeri_pari_inferiori_NUM.C
+++ Iterazioni/20241116_numeri_pari_inferiori_NUM.C
@@ -1,42 +1,29 @@
 /*scrivi un programma che legge un numero NUM strettamente positivo e visualizza tutti i numeri 
 pari inferiori a tale numero fino a 0.*/
 
-#include <stdio.h>
-int main(){
+    #include <stdio.h>
+    int main(){
 
-    int num=0;
-    int N1=0;
-    int cnt=0;
-    int numinf=0;
+    int NUM=0;
+    int i=NUM - 1;
 
-    printf("inserisci un numero strettamente positivo: ");
-    scanf("%d", &num);
-
-    while(num!=0){
-
-        if(cnt==1){
-            num==N1;
+    do {
+        printf("Inserisci un numero strettamente positivo: ");
+        scanf("%d", &NUM);
+        if (NUM <= 0) {
+            printf("Il numero deve essere strettamente positivo. Riprova.\n");
         }
-        else{
-            num==numinf;
+    } while (NUM <= 0);
+
+    printf("Numeri pari inferiori a %d fino a 0:\n", NUM);
+
+    while (i >= 0) {
+        if (i % 2 == 0) {
+            printf("%d ", i);
         }
+        i--; 
 
-        printf("il numero inserito è: %d\n", num);
-
-        printf("inserisci un numero: ");
-        scanf("%d", &num);
-
-        cnt++;
-    }
-
-    if(numinf%2==0){
-        while(numinf!=0){
-            printf("%d", numinf);
-        }
-    }
-    else{
-        printf("numero non valido.");
-    }
-
+    printf("\n");
+}
     return 0;
 }
