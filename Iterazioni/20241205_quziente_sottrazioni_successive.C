d977603331c19681d2eece084cf877828644d26c
miottopietro <pietro.miotto@galileiostiglia.edu.it>
2024-12-05 10:18:10 +0000

    \non finito

--- Iterazioni/20241205_quziente_sottrazioni_successive.C
+++ Iterazioni/20241205_quziente_sottrazioni_successive.C
@@ -5,18 +5,31 @@ int main(){
 
     int num1=0;
     int num2=0;
-    int cnt=0;
-    int resto=0;
+    int q=0;
+    int r=0;
+    int temp=0;
 
     do{
         printf("inserisci il primo valore: ");
-    }while(num1<0);
+        scanf("%d", &num1);
+    }while(num1<=0);
 
     do{
         printf("inserisci il secondo valore: ");
-    }while(num2<0);
+        scanf("%d", &num2);
+    }while(num2<=0);
 
-    for(int i=0; )
+    if(num2<num1){
+       temp=num1;
+       num1=num2;
+       num2=temp;  
+    }
+    r=num1;
+    while(r>=num2){
+        r-=num2;
+        q++;
+    }
+    printf("il quoziente è: %d con resto %d.", q, r);
 
     return 0;
 }
