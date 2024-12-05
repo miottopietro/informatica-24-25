634cd53c6b4bb08aa29e59984177b516db8e64a8
miottopietro <pietro.miotto@galileiostiglia.edu.it>
2024-12-05 09:20:38 +0000

    hhh

---
+++ Iterazioni/20241205_quziente_sottrazioni_successive.C
@@ -0,0 +1,22 @@
+/*calcolare il quoziente fra due numeri applicando il metodo delle sottrazioni successive*/
+
+#include <stdio.h>
+int main(){
+
+    int num1=0;
+    int num2=0;
+    int cnt=0;
+    int resto=0;
+
+    do{
+        printf("inserisci il primo valore: ");
+    }while(num1<0);
+
+    do{
+        printf("inserisci il secondo valore: ");
+    }while(num2<0);
+
+    for(int i=0; )
+
+    return 0;
+}
