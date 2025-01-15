49093fae3691ded1df79470f8a2b8a824f373c5f
miottopietro <pietro.miotto@galileiostiglia.edu.it>
2025-01-15 09:10:47 +0000

    verifica

---
+++ Iterazioni/20250113_calcolo_media_pagella.C
@@ -0,0 +1,12 @@
+/*scrivi un programma che effettua il calcolo della media
+dei voti della pagella, inserendoli uno alla volta e chiedendo
+a ogni inserimento di un numero se i voti da inserire sono
+terminati accettando come risposta S oppure N*/
+
+#include <stdio.h>
+int main(){
+
+    
+
+    return 0;
+}
