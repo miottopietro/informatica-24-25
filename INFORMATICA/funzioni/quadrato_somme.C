4a80c7f25c8250c1c09c5367a84f9c5c77a728ec
miottopietro <pietro.miotto@galileiostiglia.edu.it>
2025-02-24 15:28:48 +0000

    e

--- INFORMATICA/funzioni/quadrato_somme.C
+++ INFORMATICA/funzioni/quadrato_somme.C
@@ -9,16 +9,29 @@ _esempio: N = 5; N2 = 1+3+5+7+9 = 25.
 */
 
 #include <stdio.h>
+
+void quadrato(int *_num, int *_somma);
+
 int main(){
-
-    int num=0;
-
-    do{
-        printf("inserisci un numero: ");
-        scanf("%d", &num);
-    }while(num<=0);
-
-
-
-    return 0;
+	int num=0;
+	int somma=0;
+	
+	do{
+		printf("inserisci un valore: ");
+		scanf("%d", &num);
+	}while(num<0);
+	
+	quadrato(&num, &somma);
+	
+	printf("il quadrato di %d vale: %d", num, somma);
+	
+	return 0;
+}
+void quadrato(int *_num, int *_somma){
+	int dispari=1;
+	
+	for(int i=1; i<=*_num; i++){
+		*_somma+=dispari;
+		dispari+=2;
+	}
 }
