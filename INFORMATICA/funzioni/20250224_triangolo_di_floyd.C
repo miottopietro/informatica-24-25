ae37d3844408b1c30912e1229bbca0e9edf2fec0
miottopietro <pietro.miotto@galileiostiglia.edu.it>
2025-03-05 21:23:46 +0000

    triangolo di floyd

--- INFORMATICA/funzioni/20250224_triangolo_di_floyd.C
+++ INFORMATICA/funzioni/20250224_triangolo_di_floyd.C
@@ -9,3 +9,32 @@ N=5
 11 12 13 14 15
 */
 
+#include <stdio.h>
+
+void floyd(int _numRighe);
+
+int main(){
+
+    int numRighe=0;
+
+    do{
+        printf("inserisci il numero di righe: ");
+        scanf("%d", &numRighe);
+    }while(numRighe<=0);
+
+    floyd(numRighe);
+
+    return 0;
+}
+
+void floyd(int _numRighe){
+    int cnt=1;
+
+    for(int i=1; i<=_numRighe; i++){
+        for(int j=1; j<=i; j++){
+            printf("%d ", cnt);
+            cnt++;
+        }
+        printf("\n");
+    }
+}
