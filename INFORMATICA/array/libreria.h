0cf85cca78874dd7370171c22c76d693981aaafa
miottopietro <pietro.miotto@galileiostiglia.edu.it>
2025-04-22 14:13:33 +0000

    libreria h

--- INFORMATICA/array/libreria.h
+++ INFORMATICA/array/libreria.h
@@ -1,4 +1,10 @@
 /**
+ * Scambia due variabili intere
+ * @param int* variabile 1 passata per riferimento
+ * @param int* variabile 2 passata per riferimento
+ */
+void scambia(int *var1, int *var2);
+/**
  * funzione che stampa gli elementi di un vettore.
  * @param int [] vettore di interi 
  * @param int dimensione del vettore 
@@ -53,3 +59,29 @@ int ricercaBinaria(int array[], int dim, int estInf, int estSup, int valore);
  * Funzione che riempie un vettore di dimensione dim con numeri casuali in ordine crescente
  */
 void riempiVettoreOrdinatoCasuale(int vett[], int dim, int minimo, int massimo);
+
+/**
+ * funzione che ordina un vettore
+ * @param int[]vettore in ingresso
+ * @param int dimensione del vettore
+ */
+
+void bubbleSort(int vett[], int dim);
+
+/**
+ * funzione che ordina un vettore
+ * @param int[]vettore in ingresso
+ * @param int dimensione del vettore
+ */
+
+ void selectionSort(int vett[], int dim);
+
+/**
+ * funzione ricorsiva che ordina un vettore 
+ * @param int[]vettore in ingresso
+ * @param int dimensione totale del vettore (serve per stampare il vettore ad ogni ricorsione)
+ * @param int indice dell'elemento iniziale del vettore da ordinare
+ * @param int indice dell'elemento finale del vettore da ordinare
+ */
+
+ void quickSort(int v[], int dim, int inizio, int fine);
