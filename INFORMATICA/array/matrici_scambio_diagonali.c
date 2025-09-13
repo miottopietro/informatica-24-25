e16279cb115d105f476b3e05e0ba15b43bc3e8dc
miottopietro <pietro.miotto@galileiostiglia.edu.it>
2025-09-13 13:02:51 +0000

    matrici scambio diagonale

---
+++ INFORMATICA/array/matrici_scambio_diagonali.c
@@ -0,0 +1,51 @@
+//Data una matrice di valori scambiare gli elementi della diagonale principale con la diagonale secondaria.
+
+#include <stdio.h>
+
+int main() {
+    int matrice[10][10] = {0};
+    int rig = 0;
+    int col = 0;
+    int i, j, temp;
+
+    printf("Inserisci il numero di righe (max 10): ");
+    scanf("%d", &rig);
+    printf("Inserisci il numero di colonne (max 10): ");
+    scanf("%d", &col);
+
+    // Input della matrice
+    printf("Inserisci gli elementi della matrice:\n");
+    for (i=0; i<rig; i++) {
+        for (j=0; j<col; j++) {
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%d", &matrice[i][j]);
+        }
+    }
+
+    // Output della matrice originale
+    printf("Matrice originale:\n");
+    for (i=0; i<rig; i++) { 
+        for (j=0; j<col; j++) {
+            printf("%d\t", matrice[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Scambio delle diagonali
+    for (i=0; i<rig; i++) {
+        temp = matrice[i][i];
+        matrice[i][i] = matrice[i][col-i-1];
+        matrice[i][col-i-1] = temp;
+    }
+
+    // Output della matrice modificata
+    printf("Matrice dopo lo scambio delle diagonali:\n");
+    for (i=0; i<rig; i++) {
+        for (j=0; j<col; j++) {
+            printf("%d\t", matrice[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
