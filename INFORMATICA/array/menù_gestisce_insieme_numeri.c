cb03b9bdd92b3617e2fa0c3840b86e5232d0e0c0
miottopietro <pietro.miotto@galileiostiglia.edu.it>
2025-10-09 10:42:33 +0000

    fgets finito

---
+++ INFORMATICA/array/menù_gestisce_insieme_numeri.c
@@ -0,0 +1,29 @@
+/*
+ Scrivi un programma in C che gestisca un insieme di numeri interi usano un array dinamico.
+Il programma deve permettere, tramite menu:
+1. Aggiungere un numero (espandendo l'array con realloc)
+2. Visualizzare tutti inumeri
+3. Cercare un numero specifico
+4. Ordinare i numeri in ordine crescente
+5. Eliminare un numero scelto
+6. Uscire
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+d
+int* creaVett(int _dim){
+    for(int i=0; i<_dim; i++){
+
+    }
+}
+
+int main(){
+
+    int *vettore=NULL;
+    int dim=0;
+
+    printf("---MENU'---\n1. Aggiungere un numero (espandendo l'array con realloc)\n2. Visualizzare tutti inumeri\n3. Cercare un numero specifico\n4. Ordinare i numeri in ordine crescente\n5. Eliminare un numero scelto\n6. Uscire");
+
+    return 0;
+}
---
+++ INFORMATICA/stringhe/fgets.c
@@ -0,0 +1,17 @@
+/*funzione fgets*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+    char buffer[20];
+
+    printf("inserisci una stringa: ");
+    
+    if(fgets(buffer, 20, stdin)!= NULL){
+        printf("hai inserito: %s", buffer);
+    }
+
+    return 0;
+}
