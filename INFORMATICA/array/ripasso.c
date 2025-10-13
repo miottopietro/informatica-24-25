039ed8277b5484b7ccb71d7abf7d9b454e2f9be3
miottopietro <pietro.miotto@galileiostiglia.edu.it>
2025-10-13 15:51:10 +0000

    ripasso non finijtio

--- INFORMATICA/array/ripasso.c
+++ INFORMATICA/array/ripasso.c
@@ -1,11 +1,11 @@
-    #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int* creaArray(int _dim){
     int* _vettore=(int*)malloc(_dim*sizeof(int));
 
     if(_vettore==NULL){
-        printf("ahi ahi la allocazione stupida.");
+        printf("errore nell'allocazione.");
         exit(1);
     }
     return _vettore;
@@ -16,6 +16,31 @@ int* inserisciValori(int *_vettore, int _dim){
         printf("inserisci il valore in posizione %d: ", i+1);
         scanf("%d", &_vettore[i]);
     }
+    return _vettore;
+}
+
+int* aumentaDimensione(int *_vettore, int *_dim){
+    int nuovaDim=0;
+
+    do{
+        printf("inserisci la nuova dimensione: ");
+        scanf("%d", &nuovaDim);
+    }while(nuovaDim<=*_dim);
+
+    _vettore = (int*)realloc(_vettore, nuovaDim * sizeof(int));
+
+    if(_vettore==NULL){
+        printf("errore nell' allocazione.");
+        exit(1);
+    }
+
+    
+    for(int i=*_dim; i<nuovaDim; i++) {
+        printf("inserisci il valore in posizione %d: ", i+1);
+        scanf("%d", &_vettore[i]);
+    }
+
+    *_dim = nuovaDim;
     return _vettore;
 }
 
@@ -40,8 +65,9 @@ int main(){
         printf("menù:\n");
         printf("1: crea array\n");
         printf("2: inserisci valori\n");
-        printf("3: stampa array\n");
-        printf("4: esci dal programma\n");
+        printf("3: aumenta la dimensione\n");
+        printf("4: stampa array\n");
+        printf("5: esci dal programma\n");
 
         printf("scelta: ");
         scanf("%d", &scelta);
@@ -71,6 +97,16 @@ int main(){
 
                 if(vettore==NULL){
                     printf("prima devi creare l'array.\n");
+                }else{
+                    vettore=aumentaDimensione(vettore, &dim);
+                }
+
+            break;
+
+            case 4:
+
+                if(vettore==NULL){
+                    printf("prima devi creare l'array.\n");
                 }
                 else{
                     stampaVettore(vettore, dim);
@@ -78,18 +114,18 @@ int main(){
 
             break;
 
-            case 4:
+            case 5:
 
-            printf("sei uscito dal programma stupido.");
+            printf("sei uscito dal programma.");
 
             break;
 
             default:
-            printf("le scelte vanno da 1 a 4 stupido.");
+            printf("le scelte vanno da 1 a 4.\n");
 
         }
 
-    }while(scelta!=4);
+    }while(scelta!=5);
 
     free(vettore);
 
